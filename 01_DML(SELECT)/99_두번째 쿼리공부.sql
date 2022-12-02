9c0dc92f8ede18535ce216966d3baf522566e60c
pyjhoop <pyjhoop@naver.com>
2022-12-02 15:12:58 +0900

    COMMIT
    COMMIT

---
+++ 01_DML(SELECT)/99_두번째 쿼리공부.sql
@@ -0,0 +1,41 @@
+-- 먼저 저장
+---------------------------------QUIZ 1------------------------------------------
+-- ROWNUM을 활용해서 급여가 가장 높은 5명을 조회하고 싶었으나, 제대로 조회가 안됐음!!
+-- 이때 작성된 SQL문이 아래와 같음.
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM EMPLOYEE
+WHERE ROWNUM <= 5
+ORDER BY SALARY DESC;
+
+-- 어떤 문제점이 있는지, 해결된 SQL문 작성
+--1. ORDER BY 가 가장 마지막에 적용되기에 ROWNUM의 순서가 섞임
+--2. 먼저 정렬을 진행하고 그다음 ROWNUM을 추출해야함.
+
+SELECT ROWNUM, E.*
+FROM (SELECT EMP_NAME, SALARY
+      FROM EMPLOYEE
+      ORDER BY 2 DESC) E
+WHERE ROWNUM <=5;
+---------------------------------QUIZ 2------------------------------------------
+-- 부서별 평균 급여가 270만원을 초과하는 부서들에 대해 (부서코드, 부서별 총급여합, 부서별 평균급여, 부서별 사원수)
+-- 이때 작성된 SQL문이 아래와 같음.
+SELECT DEPT_CODE, SUM(SALARY) 총합, FLOOR(AVG(SALARY)) 평균, COUNT(*) 인원수
+FROM EMPLOYEE
+WHERE SALARY >2700000
+GROUP BY DEPT_CODE
+ORDER BY 1;
+
+--어떤 문제점이 있는지, 해결된 SQL문작성
+-- 부서별 평균 급여가 270만원 초과인데 WHERE절로 사원의 급여가 270만원 초과라는 조건식을 사용함.
+-- 조건식을 HAVING절에 넣어야함.
+
+SELECT DEPT_CODE, SUM(SALARY) 총합, FLOOR(AVG(SALARY)) 평균, COUNT(*) 인원수
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) > 2700000
+ORDER BY 1;
+
+
+
+
+
